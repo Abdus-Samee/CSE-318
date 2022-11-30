39906c7d8c3637809124e6c825e43cd5b1fa7da6
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-11-30 18:52:41 +0600

    Create CompareNode.java

---
+++ 1. N-Puzzle/CompareNode.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class CompareNode implements Comparator<Node> {
+    @Override
+    public int compare(Node node, Node t1) {
+        //f(n)=g(n)+h(n)
+        return 0;
+    }
+}
