40ac3bb78a86dbd1f77df66f5b45a2cd550c2ece
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-03 19:31:27 +0600

    Update Node.java

--- 1. N-Puzzle/Node.java
+++ 1. N-Puzzle/Node.java
@@ -1,5 +1,7 @@
 public class Node {
     int moves;
+    int distHamming;
+    int distManhattan;
     String[][] grid;
     Node prev;
 
@@ -7,13 +9,67 @@ public class Node {
         moves = m;
         grid = g;
         prev = p;
+        calculateDistHamming();
     }
 
     public void setMoves(int m){ moves = m; }
     public void setGrid(String[][] g){ grid = g; }
     public void setPrev(Node p){ prev = p; }
+    public void setDistHamilton(int distHamming){ this.distHamming = distHamming; }
+    public void setDistManhattan(int distManhattan){ this.distManhattan = distManhattan; }
 
-    public int getMoves(){ return moves; }
-    public String[][] getGrid(){ return grid; }
-    public Node getPrev(){ return prev; }
+    public int getHammingHeuristic(){ return moves + distHamming; }
+    public int getManhattanHeuristic(){ return moves + distManhattan; }
+
+    public void calculateDistHamming(){
+        int ans = 0;
+        int k = 1;
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j].equals("*")) continue;
+                else if(!grid[i][j].equals(String.valueOf(k))) ans++;
+
+                k++;
+            }
+        }
+
+        distHamming = ans;
+    }
+
+    public void calculateDistManhattan(){
+
+    }
+
+    public void printGrid(){
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                System.out.printf(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public boolean equalNode(String[][] g){
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid.length; j++){
+                if(!grid[i][j].equals(g[i][j])) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public String toString(){
+        String res = "";
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid.length; j++){
+                res += grid[i][j] + " ";
+            }
+        }
+
+        return res;
+    }
 }
