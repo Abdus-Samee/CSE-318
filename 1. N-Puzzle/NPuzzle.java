6b33d50ba90ef87f1462599e8d9a77ed0ae8bb95
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-04 11:56:42 +0600

    Update NPuzzle.java

--- 1. N-Puzzle/NPuzzle.java
+++ 1. N-Puzzle/NPuzzle.java
@@ -14,113 +14,14 @@ public class NPuzzle {
 
         if(!solvable(grid)) System.out.println("Not solvable");
         else{
-            System.out.println("Solvable");
-            PriorityQueue<Node> pq = new PriorityQueue<>(new CompareNode());
-            HashSet<String> explored = new HashSet<>();
+            System.out.println("Solvable\n");
+            System.out.println("-----Hamming Distance Heuristic-----\n");
+            Solver solver = new Solver(k, 1, grid);
+            solver.solve();
 
-            Node root = new Node(0, grid, null);
-            pq.add(root);
-            explored.add(root.toString());
-
-            int t = 1;
-            String[][] goal = new String[k][k];
-            for(int i = 0; i < goal.length; i++){
-                for(int j = 0; j < goal[i].length; j++){
-                    goal[i][j] = String.valueOf(t++);
-                }
-            }
-            goal[k-1][k-1] = "*";
-
-            int exploredNodes = 0, expandedNodes = 1;
-            while(!pq.isEmpty()){
-                Node n = pq.poll();
-                exploredNodes++;
-                if(n.equalNode(goal)) {
-                    n.printGrid();
-                    System.out.println("Total moves: " + n.moves);
-                    break;
-                }
-                n.printGrid();
-
-                int x = 0,y = 0;
-                for(int i = 0; i < k; i++){
-                    for(int j = 0; j < k; j++){
-                        if(n.grid[i][j].equals("*")){
-                            x = i;
-                            y = j;
-                            break;
-                        }
-                    }
-                }
-
-                if(x > 0){
-                    String[][] newGrid = new String[k][k];
-                    for(int i = 0; i < k; i++){
-                        for(int j = 0; j < k; j++){
-                            newGrid[i][j] = n.grid[i][j];
-                        }
-                    }
-                    newGrid[x][y] = newGrid[x-1][y];
-                    newGrid[x-1][y] = "*";
-                    Node newNode = new Node(n.moves+1, newGrid, n);
-                    if(!explored.contains(newNode.toString())){
-                        pq.add(newNode);
-                        explored.add(newNode.toString());
-                        expandedNodes++;
-                    }
-                }
-                if(x < k-1){
-                    String[][] newGrid = new String[k][k];
-                    for(int i = 0; i < k; i++){
-                        for(int j = 0; j < k; j++){
-                            newGrid[i][j] = n.grid[i][j];
-                        }
-                    }
-                    newGrid[x][y] = newGrid[x+1][y];
-                    newGrid[x+1][y] = "*";
-                    Node newNode = new Node(n.moves+1, newGrid, n);
-                    if(!explored.contains(newNode.toString())){
-                        pq.add(newNode);
-                        explored.add(newNode.toString());
-                        expandedNodes++;
-                    }
-                }
-                if(y > 0){
-                    String[][] newGrid = new String[k][k];
-                    for(int i = 0; i < k; i++){
-                        for(int j = 0; j < k; j++){
-                            newGrid[i][j] = n.grid[i][j];
-                        }
-                    }
-                    newGrid[x][y] = newGrid[x][y-1];
-                    newGrid[x][y-1] = "*";
-                    Node newNode = new Node(n.moves+1, newGrid, n);
-                    if(!explored.contains(newNode.toString())){
-                        pq.add(newNode);
-                        explored.add(newNode.toString());
-                        expandedNodes++;
-                    }
-                }
-                if(y < k-1){
-                    String[][] newGrid = new String[k][k];
-                    for(int i=0;i<k;i++){
-                        for(int j=0;j<k;j++){
-                            newGrid[i][j] = n.grid[i][j];
-                        }
-                    }
-                    newGrid[x][y] = newGrid[x][y+1];
-                    newGrid[x][y+1] = "*";
-                    Node newNode = new Node(n.moves+1, newGrid, n);
-                    if(!explored.contains(newNode.toString())){
-                        pq.add(newNode);
-                        explored.add(newNode.toString());
-                        expandedNodes++;
-                    }
-                }
-            }
-
-            System.out.println("Number of explored nodes: " + exploredNodes);
-            System.out.println("Number of expanded nodes: " + expandedNodes);
+            System.out.println("-----Manhattan Distance Heuristic-----\n");
+            solver = new Solver(k, 2, grid);
+            solver.solve();
         }
     }
 
