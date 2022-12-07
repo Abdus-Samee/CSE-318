8e668fec130c7d609b4f657fad1ff9bb6e6fb5b3
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-07 18:30:23 +0600

    Update CompareNode.java

--- 1. N-Puzzle/CompareNode.java
+++ 1. N-Puzzle/CompareNode.java
@@ -1,11 +1,20 @@
 import java.util.Comparator;
 
 public class CompareNode implements Comparator<Node> {
+    int choice;
+
+    public CompareNode(int choice){ this.choice = choice; }
+
     @Override
     public int compare(Node n1, Node n2) {
         //f(n)=g(n)+h(n)
-        if(n1.getHammingHeuristic() > n2.getHammingHeuristic()) return 1;
-        else if(n1.getHammingHeuristic() < n2.getHammingHeuristic()) return -1;
+        if(choice == 1){
+            if(n1.getHammingHeuristic() > n2.getHammingHeuristic()) return 1;
+            else if(n1.getHammingHeuristic() < n2.getHammingHeuristic()) return -1;
+        }else{
+            if(n1.getManhattanHeuristic() > n2.getManhattanHeuristic()) return 1;
+            else if(n1.getManhattanHeuristic() < n2.getManhattanHeuristic()) return -1;
+        }
 
         return 0;
     }
