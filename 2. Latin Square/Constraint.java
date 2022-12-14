fb064a8c4919f61a0dc103cace10e57c7c31928e
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-14 18:49:05 +0600

    Corrected methods

--- 2. Latin Square/Constraint.java
+++ 2. Latin Square/Constraint.java
@@ -2,12 +2,15 @@ public class Constraint {
     Variable v1;
     Variable v2;
     boolean integrity;
+    int c = 0;
+
+    public Constraint(){}
 
     public Constraint(Variable v1, Variable v2){
         this.v1 = v1;
         this.v2 = v2;
 
-        this.integrity = checkIntegrity(this.v1, this.v2);
+        //this.integrity = checkIntegrity(this.v1, this.v2);
     }
 
     public boolean checkIntegrity(Variable v1, Variable v2){
@@ -16,17 +19,23 @@ public class Constraint {
         return false;
     }
 
-    public boolean holds(Assignment assignment){
-        int i = 0;
-        int n = assignment.mp.size();
-        Variable[] ar = new Variable[n];
-        for(Variable v : assignment.mp.keySet()){
-            ar[i++] = v;
+    public boolean consistentAssignment(Variable v, int val, Variable[][] varGrid, Assignment assignment){
+        int r = v.row;
+        int c = v.col;
+        for(int j = 0; j < varGrid.length; j++){
+            if((c != j) && assignment.getVal(varGrid[r][j]) == val) return false;
+        }
+        for(int i = 0; i < varGrid.length; i++){
+            if((r != i) && assignment.getVal(varGrid[i][c]) == val) return false;
         }
 
-        for(i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(!checkIntegrity(ar[i], ar[j])) return false;
+        return true;
+    }
+
+    public boolean holds(Assignment assignment, Variable[][] varGrid){
+        for(int i = 0; i < varGrid.length; i++){
+            for(int j = 0; j < varGrid[i].length; j++){
+                if(assignment.getVal(varGrid[i][j]) == 0) return false;
             }
         }
 
