c84bdaedaa798ad48b6a36b35e510b7df7aa4a1e
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-14 18:50:11 +0600

    Update Variable.java

--- 2. Latin Square/Variable.java
+++ 2. Latin Square/Variable.java
@@ -3,13 +3,38 @@ import java.util.List;
 
 public class Variable {
     List<Integer> domain;
+    int n;
     int val;
+    int row;
+    int col;
     boolean set;
 
-    public Variable(int n){
+    public Variable(int n, int row, int col){
+        this.n = n;
+        this.row = row;
+        this.col = col;
+        this.set = false;
         this.domain = new ArrayList<>();
 
         for(int i = 1; i <= n; i++) this.domain.add(i);
+    }
+
+    public void setVal(int val){
+        this.val = val;
+        this.set = true;
+    }
+
+    public void setDomain(List<Integer> domain) { this.domain = domain; }
+
+    public List<Variable> findNeighbours(Variable[][] varGrid){
+        List<Variable> ans = new ArrayList<>();
+
+        if(row-1 >= 0) ans.add(varGrid[row-1][col]);
+        if(row+1 < n) ans.add(varGrid[row+1][col]);
+        if(col-1 >= 0) ans.add(varGrid[row][col-1]);
+        if(col+1 < n) ans.add(varGrid[row][col+1]);
+
+        return ans;
     }
 }
 
