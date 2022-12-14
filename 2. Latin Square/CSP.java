97bdd2f32da13f4fb74b7899d1a963380a3085c9
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-12-14 18:47:50 +0600

    Added updateDomain() for forward checking

--- 2. Latin Square/CSP.java
+++ 2. Latin Square/CSP.java
@@ -2,15 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSP {
+    Constraint constraint;
+    Variable[][] varGrid;
     List<Variable> variableList;
-    List<Constraint> constraintList;
 
-    public CSP(){
+    public CSP(int n){
+        this.varGrid = new Variable[n][n];
         this.variableList = new ArrayList<>();
-        this.constraintList = new ArrayList<>();
     }
 
-    public void addVariable(Variable variable) { this.variableList.add(variable); }
-
-    public void addConstraint(Constraint constraint) { this.constraintList.add(constraint); }
+    public void updateDomain(){
+        for(Variable v : variableList){
+            for(int j = 0; j < varGrid.length; j++){
+                Variable o = varGrid[v.row][j];
+                if(o.val != 0) v.domain.remove(o.val);
+            }
+            for(int i = 0; i < varGrid.length; i++){
+                Variable o = varGrid[i][v.col];
+                if(o.val != 0) v.domain.remove(o.val);
+            }
+        }
+    }
 }
