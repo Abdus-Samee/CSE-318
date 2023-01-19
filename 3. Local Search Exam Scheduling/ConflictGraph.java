f9e5785f8283c4c349a40b03afd1ad2c40512eab
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-19 14:55:38 +0600

    Update ConflictGraph.java

--- 3. Local Search Exam Scheduling/ConflictGraph.java
+++ 3. Local Search Exam Scheduling/ConflictGraph.java
@@ -31,24 +31,49 @@ public class ConflictGraph {
             }else conflicting_courses.add(c);
         }
 
-        Heuristic heuristic = new Heuristic(constructive_heuristic, conflicting_courses);
+        Heuristic heuristic = new Heuristic(constructive_heuristic);
 
         //assign days
-        while(conflicting_courses.size() > 0){
-            Course c = heuristic.getNextCourse();
-            conflicting_courses.remove(conflicting_courses.indexOf(c));
-            c.assignDay(days);
-            resetDegreeNonConflictingCourses(c, conflicting_courses);
-
-            //remove c from conflict of all other courses
-            for(Course course : conflicting_courses){
-                course.conflicts.remove(c);
+        if(constructive_heuristic.equals("largest degree")){
+            while(conflicting_courses.size() > 0){
+                Course c = heuristic.getNextCourse(conflicting_courses);
+                conflicting_courses.remove(conflicting_courses.indexOf(c));
+                c.assignDay(days);
+                resetDegreeNonConflictingCourses(c, conflicting_courses);
+    
+                //remove c from conflict of all other courses
+                for(Course course : conflicting_courses){
+                    course.conflicts.remove(c);
+                }
+    
+                days++;
             }
-
-            days++;
+        }else if(constructive_heuristic.equals("saturation degree")){
+                boolean[] used = new boolean[conflicting_courses.size()];
+                while(conflicting_courses.size() > 0){
+                    Course c = heuristic.getNextCourse(conflicting_courses);
+                    //System.out.println("selected: " + c.courseId + "index: " + conflicting_courses.indexOf(c) + "\n");
+                    conflicting_courses.remove(conflicting_courses.indexOf(c));
+                    for(Course course : c.conflicts){
+                        if(course.day != -1) used[course.day] = true;
+                    }
+                    for(int i = 1; i <= used.length; i++){
+                        if(!used[i]){
+                            //System.out.println("Assigning day " + i + " to " + c.courseId);
+                            c.assignDay(i);
+                            break;
+                        }
+                    }
+                    for(Course course : c.conflicts){
+                        if(course.day != -1) used[course.day] = false;
+                    }
+                }
+        }else if(constructive_heuristic.equals("largest enrollment")){
+            
         }
     }
 
+    //add the non-conflicting courses from conflicting_courses list to the same day
     public void resetDegreeNonConflictingCourses(Course course, List<Course> conflicting_courses) {
         List<Course> non_conflicting_courses = new ArrayList<>();
         for(Course c : conflicting_courses){
