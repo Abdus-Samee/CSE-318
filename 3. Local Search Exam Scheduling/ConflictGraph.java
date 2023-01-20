6d27d6ec5cce0e5abfa3aab62429e0b255df4a0d
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2023-01-20 22:00:03 +0600

    Added penalty calculation

--- 3. Local Search Exam Scheduling/ConflictGraph.java
+++ 3. Local Search Exam Scheduling/ConflictGraph.java
@@ -3,13 +3,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ConflictGraph {
-    int days = 1;
+    int days;
+    double penalty_after_constructive;
+    double penalty_after_kempe;
+    double penalty_after_pair_swap;
     String constructive_heuristic;
+    String penalty_strategy;
     List<Course> courses;
     List<Student> students;
 
-    public ConflictGraph(String constructive_heuristic, List<Course> courses, List<Student> students) {
+    public ConflictGraph(String constructive_heuristic, String penalty_strategy, List<Course> courses, List<Student> students) {
+        this.days = 1;
+        this.penalty_after_constructive = 0;
+        this.penalty_after_kempe = 0;
+        this.penalty_after_pair_swap = 0;
         this.constructive_heuristic = constructive_heuristic;
+        this.penalty_strategy = penalty_strategy;
         this.courses = courses;
         this.students = students;
     }
@@ -24,42 +33,10 @@ public class ConflictGraph {
             }
         }
 
-        //remove non-conflicting courses
-        //List<Course> conflicting_courses = new ArrayList<>();
-        // for(Course c : courses){
-        //     if(c.getEdgeCount() == 0){
-        //         c.assignDay(days);
-        //     }else conflicting_courses.add(c);
-        // }
-
         Heuristic heuristic = new Heuristic(constructive_heuristic);
 
-        //assign days
-        if(constructive_heuristic.equals("largest degree")){
-            boolean availableColor[] = new boolean[courses.size()+1];
-            Arrays.fill(availableColor, true);
-            
-            while(courses.size() > 0){
-                Course c = heuristic.getNextCourse(courses);
-                courses.remove(courses.indexOf(c));
-                //c.assignDay(days);
-                //resetDegreeNonConflictingCourses(c, courses);
-    
-                for(Course course : c.conflicts){
-                    if(course.day != -1) availableColor[course.day] = false;
-                }
-    
-                for(int i = 1; i <= courses.size(); i++){
-                    if(availableColor[i]){
-                        c.assignDay(i);
-                        days = Math.max(days, i);
-                        break;
-                    }
-                }
-
-                Arrays.fill(availableColor, true);
-            }
-        }else if(constructive_heuristic.equals("saturation degree")){
+        //assign days to courses
+        if(constructive_heuristic.equals("saturation degree")){
                 boolean[] used = new boolean[courses.size()+1];
                 for(Course course : courses) course.d = course.getEdgeCount();
 
@@ -87,39 +64,13 @@ public class ConflictGraph {
                         if(course.day == -1) course.d--;
                     }
                 }
-        }else if(constructive_heuristic.equals("largest enrollment")){
-            boolean[] used = new boolean[courses.size()+1];
-            while(courses.size() > 0){
-                Course c = heuristic.getNextCourse(courses);
-                courses.remove(courses.indexOf(c));
-                //c.assignDay(days);
-                //resetDegreeNonConflictingCourses(c, conflicting_courses);
-    
-                for(Course course : c.conflicts){
-                    if(course.day != -1) used[course.day] = true;
-                }
-    
-                for(int i = 1; i <= courses.size(); i++){
-                    if(!used[i]){
-                        c.assignDay(i);
-                        days = Math.max(days, i);
-                        break;
-                    }
-                }
-                
-                for(Course course : c.conflicts){
-                    if(course.day != -1) used[course.day] = false;
-                }
-            }
-        }else if(constructive_heuristic.equals("random")){
+        }else{
             boolean availableColor[] = new boolean[courses.size()+1];
             Arrays.fill(availableColor, true);
-
+            
             while(courses.size() > 0){
                 Course c = heuristic.getNextCourse(courses);
                 courses.remove(courses.indexOf(c));
-                //c.assignDay(days);
-                //resetDegreeNonConflictingCourses(c, conflicting_courses);
     
                 for(Course course : c.conflicts){
                     if(course.day != -1) availableColor[course.day] = false;
@@ -136,6 +87,69 @@ public class ConflictGraph {
                 Arrays.fill(availableColor, true);
             }
         }
+
+        if(this.penalty_strategy.equals("exponential")){
+            this.penalty_after_constructive = getExponentialPenalty();
+            execKempeChain();
+            this.penalty_after_kempe = getExponentialPenalty();
+            execPairSwap();
+            this.penalty_after_pair_swap = getExponentialPenalty();
+        }
+    }
+
+    public double getLinearPenalty(){
+        double penalty = 0;
+
+        for(Student s : students){
+            List<Course> courses = s.courses;
+
+            for(Course c1 : courses){
+                for(Course c2 : courses){
+                    if(!c1.courseId.equals(c2.courseId)){
+                        int diff = Math.abs(c1.day - c2.day);
+                        if(diff == 0) penalty += 10;
+                        else if(diff == 1) penalty += 8;
+                        else if(diff == 2) penalty += 6;
+                        else if(diff == 3) penalty += 4;
+                        else if(diff == 4) penalty += 2;
+                    }
+                }
+            }
+        }
+        
+        return penalty/students.size();
+    }
+
+    public double getExponentialPenalty(){
+        double penalty = 0;
+
+        for(Student s : students){
+            List<Course> courses = s.courses;
+
+            for(Course c1 : courses){
+                for(Course c2 : courses){
+                    if(!c1.courseId.equals(c2.courseId)){
+                        int diff = Math.abs(c1.day - c2.day);
+                        if(diff == 0) penalty += 32;
+                        else if(diff == 1) penalty += 16;
+                        else if(diff == 2) penalty += 8;
+                        else if(diff == 3) penalty += 4;
+                        else if(diff == 4) penalty += 2;
+                        else if(diff == 5) penalty += 1;
+                    }
+                }
+            }
+        }
+        
+        return penalty/students.size();
+    }
+
+    public void execKempeChain(){
+
+    }
+
+    public void execPairSwap(){
+
     }
 
     //add the non-conflicting courses from conflicting_courses list to the same day
